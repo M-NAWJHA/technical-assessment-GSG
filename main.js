6644e5164d0d1e12ba1edc51d89cfffe8566e0e7
M-NAWJHA <m.nawjha@gmail.com>
2024-11-16 15:37:54 +0200

    handel Total Price for the product by typescript

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const handelRmDuplicates = arr => {
-  const uniqueArr = [...new Set(arr)];
-  if (Array.isArray(arr)) {
-    return `Remove Duplicates: ${uniqueArr}`;
-  } else {
-    return 'The input is not array';
-  }
-};
-
-const arrString = ['a', 'b', 'c', 'd', 'e', 'b', 'c', 'd'];
-console.log(
-  `Array of String: ${arrString} => ${handelRmDuplicates(arrString)}`
-);
+(function () {
+    // handel Total Price for the product
+    var handelTotalPrice = function (arrProduct) {
+        return arrProduct.reduce(function (total, product) { return total + product.price; }, 0);
+    };
+    // array of products
+    var myProducts = [
+        { name: 'car red', price: 10 },
+        { name: 'car blue', price: 20 },
+        { name: 'car black', price: 30 },
+    ];
+    console.log("Total Price ".concat(handelTotalPrice(myProducts)));
+})();
